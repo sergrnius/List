100d94f4441c5fe28c737fc0f70e9cca05705dbf
sergrnius <aewfrh.adfh.95@bk.ru>
2022-07-01 22:48:25 +0300

    Updates

---
+++ precache-manifest.c5376ea2f7aec66422ec98c25bd336c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cf89225ea2bfbcea982",
+    "url": "/static/css/main.748ee697.chunk.css"
+  },
+  {
+    "revision": "8cf89225ea2bfbcea982",
+    "url": "/static/js/main.8cf89225.chunk.js"
+  },
+  {
+    "revision": "cf62db6b069ea415781c",
+    "url": "/static/js/1.cf62db6b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22ece41574079f92a187fde73bee2382",
+    "url": "/index.html"
+  }
+];
